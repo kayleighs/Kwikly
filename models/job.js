324079d540f42d52d9365d8bd97ee7b6f5bcb8fe
Johnny <johnrussoofficial@outlook.com>
2019-09-16 10:37:38 -0400

    building data structure

--- models/job.js
+++ models/job.js
@@ -3,6 +3,10 @@ const Schema = mongoose.Schema;
 
 const jobSchema = new Schema({
   title: { type: String, required: true },
+  employer: { type: String, required: true },
+  description: { type: String, required: true },
+  hiredWorkers: [{ type: String }],
+  address: { type: String, required: true },
   location: {
     lat : { type: String, required: true },
     lng : { type: String, required: true }
